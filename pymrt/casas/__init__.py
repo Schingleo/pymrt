124c81d76d0db5485ae6bbe0190c778db7e53cd4
Tinghui Wang <tinghui.wang@email.wsu.edu>
2018-01-21 11:16:43 -0800

    casas: Add import in __init__.py
    So that CASASDataset and CASASSite can be found in pymrt.casas directly.
    
    Signed-off-by: Tinghui Wang <tinghui.wang@email.wsu.edu>

--- pymrt/casas/__init__.py
+++ pymrt/casas/__init__.py
@@ -3,5 +3,10 @@
 This package contains classes and functions to interact with CASAS smart home
 dataset.
 """
+
+from .dataset import CASASDataset
+from .site import CASASSite
+from .sensor_type import CASASSensorType
+
 global casas_sites_dir
 casas_sites_dir = None
