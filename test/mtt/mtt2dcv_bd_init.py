d54b8212da27cc3dc4d073765fe5c8b33f210211
Tinghui Wang <tinghui.wang@email.wsu.edu>
2018-02-16 20:58:35 -0800

    Parameters for 2D GM-PHD Test
    Signed-off-by: Tinghui Wang <tinghui.wang@email.wsu.edu>

---
+++ test/mtt/mtt2dcv_bd_init.py
@@ -0,0 +1,60 @@
+import numpy as np
+from pymrt.tracking.utils import GaussianComponent
+
+# Dimensionality
+n = 2
+
+# 12 targets, 2D Constant Velocity Model
+n_targets = 12
+x_init = np.zeros((n_targets, 4))
+x_init[ 0, :] = [   0,    0,    0, -10]
+x_init[ 1, :] = [ 400, -600,  -10,   5]
+x_init[ 2, :] = [-800, -200,   20,  -5]
+x_init[ 3, :] = [ 400, -600,   -7,  -4]
+x_init[ 4, :] = [ 400, -600, -2.5,  10]
+x_init[ 5, :] = [   0,    0,  7.5,  -5]
+x_init[ 6, :] = [-800, -200,   12,   7]
+x_init[ 7, :] = [-200,  800,   15, -10]
+x_init[ 8, :] = [-800, -200,    3,  15]
+x_init[ 9, :] = [-200,  800,   -3, -15]
+x_init[10, :] = [   0,    0,  -20, -15]
+x_init[11, :] = [-200,  800,   15,  -5]
+
+# Total 100 steps
+n_steps = 100
+birth = np.zeros((n_targets, 1), np.int)
+# #target      0  1  2   3   4   5   6   7   8   9  10  11
+birth[:, 0] = [1, 1, 1, 20, 20, 20, 40, 40, 60, 60, 80, 80]
+
+death = np.zeros((n_targets, 1), np.int)
+death[:, :] = n_steps
+death[0, 0] = 70
+death[2, 0] = 70
+
+# STD of CV disturbance on state updates
+w_cov_std = 1
+# STD of CV disturbance on measurements
+r_cov_std = 5
+# Lambda parameter of clutter process - on average, 10 false-alarms per
+# observation
+poisson_lambda = 10
+
+# Birth probability
+model_birth = [
+    GaussianComponent(n=4,
+                      mean=np.array([0., 0., 0., 0.]).T,
+                      cov=np.eye(4)*100,
+                      weight=0.03),
+    GaussianComponent(n=4,
+                      mean=np.array([400., -600., 0., 0.]).T,
+                      cov=np.eye(4) * 100,
+                      weight=0.03),
+    GaussianComponent(n=4,
+                      mean=np.array([-800., -200., 0., 0.]).T,
+                      cov=np.eye(4) * 100,
+                      weight=0.03),
+    GaussianComponent(n=4,
+                      mean=np.array([-200., 800., 0., 0.]).T,
+                      cov=np.eye(4) * 100,
+                      weight=0.03),
+]
